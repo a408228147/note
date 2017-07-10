28f66132888b58f31c956f775799f1b9ea84cb93
youze <youzel@126.com>
2017-07-10 15:38:57 +0800

    Desige Method

---
+++ src/FactoryMethod/MailSender.java
@@ -0,0 +1,14 @@
+package FactoryMethod;
+
+/**
+ * Created by admin on 2017/7/10.
+ */
+
+/**
+ * create the implements class
+ */
+public class MailSender implements Sender {
+    public void send(){
+        System.out.println("This is a mailsender");
+    }
+}
---
+++ src/FactoryMethod/SendFactory.java
@@ -0,0 +1,17 @@
+package FactoryMethod;
+
+/**
+ * Created by admin on 2017/7/10.
+ */
+public class SendFactory {
+    public Sender produce(String type) {
+        if ("mail".equals(type)) {
+            return new MailSender();
+        } else if ("sms".equals(type)) {
+            return new SmsSender();
+        } else {
+            System.out.println("请输入正确的类型");
+            return null;
+        }
+    }
+}
