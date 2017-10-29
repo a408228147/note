042110dcfa3f330a292f22a2e386312b74994837
youzeliang <youzel@126.com>
2017-10-29 21:27:25 +0800

    抽象工厂类

---
+++ src/abstractFactory/FactoryProducer.java
@@ -0,0 +1,18 @@
+package abstractFactory;
+
+/**
+ * Created by youzeliang on 2017/10/29.
+ */
+public class FactoryProducer {
+    public static AbstractFactory getFactory(String choice){
+
+        if(choice.equalsIgnoreCase("SHAPE")){
+            return new ShapeFactory();
+
+        }else if(choice.equalsIgnoreCase("COLOR")){
+            return new ColorFactory();
+        }
+
+        return null;
+    }
+}
