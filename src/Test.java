3968edfbdcd98bc868972fb34f49b430b6436478
yzrds <youzel@126.com>
2018-06-26 00:18:57 +0800

    redis

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author youzeliang
+ * on 2018/6/24
+ */
+public class Test {
+    public static List<String> compareTest1() {
+        List<String> wordList = Arrays.asList("lianggzone", "spring", "summer", "autumn", "winter");
+        wordList.sort(new Comparator<String>() {
+            @Override
+            public int compare(String w1, String w2) {
+                return Integer.compare(w1.length(), w2.length());
+            }
+        });
+        return wordList;
+    }
+}
